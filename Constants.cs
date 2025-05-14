dccdbd88c36207a9524b4372f8ac903ea99ea043
Максим <maxim27800@gmail.com>
2025-05-15 00:45:56 +0300

    Налаштовано запити до АПІ на railway

--- Constants.cs
+++ Constants.cs
@@ -9,7 +9,7 @@ namespace SpaceBot
     internal class Constants
     {
         public static string BotID = "7560996597:AAE518iWQA-9gyfX3GJJGLhrbgbxo0oL3Uk";
-        public static string ApiAdress = "https://localhost:7218";
+        public static string ApiAdress = "https://spaceapi-spaceapi.up.railway.app";
         public static string ApiHost = "/api/SpacePhoto";
     }
 }
